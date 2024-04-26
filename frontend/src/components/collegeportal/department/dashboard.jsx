be6fcbb2a292bce0969f4dc5f800de2aedc50df1
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2024-04-26 19:34:27 +0530

    Bugs and Fixes

--- frontend/src/components/collegeportal/department/dashboard.jsx
+++ frontend/src/components/collegeportal/department/dashboard.jsx
@@ -17,31 +17,31 @@ const DashboardPage = () => {
           <div className=" col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between h-36">
               <div className="heading text-xl	font-normal	">Bundles Examined</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">35</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">45</div>
           </div>
 
           <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Bundles Moderated</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">35</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
           </div>
           
           <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Total Bundle</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">20</div>
           </div>
 
           <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Total Teachers</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">10</div>
           </div>
 
           <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Total Students</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">310</div>
           </div>
 
         </div>
@@ -51,14 +51,14 @@ const DashboardPage = () => {
         <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Number of Subjects</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">33</div>
           </div>
 
           
           <div className="col-span-1 total-college-registered-card card font-inter m-2 p-5
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between">
               <div className="heading text-xl	font-normal	">Total Bundles</div>
-              <div className="value font-semibold text-4xl text-text-blue pt-1.5">--</div>
+              <div className="value font-semibold text-4xl text-text-blue pt-1.5">20</div>
           </div>
 
           
@@ -66,10 +66,10 @@ const DashboardPage = () => {
           bg-white drop-shadow-2xl cursor-pointer flex flex-col justify-between ">
               <div className="heading text-xl	font-normal	">Total Students in each year</div>
               <div className="value font-semibold text-4xl text-text-blue pt-1.5 ">
-                <h1><span>FE</span> : <span>Count</span></h1>
-                <h1><span>SE</span> : <span>Count</span></h1>
-                <h1><span>TE</span> : <span>Count</span></h1>
-                <h1><span>BE</span> : <span>Count</span></h1>
+                <h1><span>FE</span> : <span>70</span></h1>
+                <h1><span>SE</span> : <span>80</span></h1>
+                <h1><span>TE</span> : <span>80</span></h1>
+                <h1><span>BE</span> : <span>80</span></h1>
               </div>
           </div>
         </div>
