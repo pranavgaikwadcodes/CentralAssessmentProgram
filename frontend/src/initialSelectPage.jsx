eb847cdd156c9c8e657983ebcc79db46d65721c8
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-05-25 13:46:01 +0530

    improvements

--- frontend/src/initialSelectPage.jsx
+++ frontend/src/initialSelectPage.jsx
@@ -3,9 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 const InitialSelectPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-purple-500 to-indigo-600">
-      <h1 className="text-white text-3xl md:text-4xl font-bold mb-8 font-inter">CENTRAL ASSESSMENT PROGRAM</h1>
-      <div className="bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg rounded-lg p-24">
+    // <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-purple-500 to-indigo-600">
+    <div className="flex flex-col justify-center items-center h-screen bg-white" style={{
+      backgroundColor: '#ffffff',
+      backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='0.2' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cuse fill='%23fcfcfc' href='%23s' y='2'/%3E%3Cuse fill='%23fcfcfc' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='2'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23f2f2f2'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23ffffff'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23efefef'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(16.65) translate(-939.94 -704.95)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E")`,
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover'
+    }} >
+
+      <h1 className=" text-3xl md:text-4xl font-extrabold mb-8 font-inter">CENTRAL ASSESSMENT PROGRAM</h1>
+      <div className="bg-black bg-opacity-5 backdrop-filter backdrop-blur-lg rounded-lg p-24">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           
           <NavLink to="adminlogin">
@@ -15,19 +22,19 @@ const InitialSelectPage = () => {
           </NavLink>
 
           <NavLink to="collegelogin">
-            <div className="bg-teal-500 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-teal-600 transition duration-300 ease-in-out">
+            <div className="bg-blue-500 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-blue-600 transition duration-300 ease-in-out">
                 COLLEGE PORTAL
             </div>
           </NavLink>
 
           <NavLink to="departmentlogin">
-            <div className="bg-orange-400 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-orange-500 transition duration-300 ease-in-out">
+            <div className="bg-blue-500 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-blue-600 transition duration-300 ease-in-out">
                 DEPARTMENT PORTAL
             </div>
           </NavLink>
 
           <NavLink to="examinerlogin">
-            <div className="bg-yellow-500 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-yellow-600 transition duration-300 ease-in-out">
+            <div className="bg-blue-500 text-white p-10 rounded-md text-center font-inter text-xl font-semibold hover:bg-blue-600 transition duration-300 ease-in-out">
                 EXAMINER PORTAL
             </div>
           </NavLink>
