68bb65eec07ff9d5e430ef375f38ef33a2bfe343
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-11-22 22:08:45 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
